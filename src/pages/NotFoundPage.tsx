5eaf791d4c8a5bd1328c4b2770be7fab3ea2b5dd
aubers <purplnay@gmail.com>
2024-06-28 17:21:42 +0200

    Add 404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,3 +1,17 @@
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
 export default function NotFoundPage() {
-  return <main>Not found page</main>;
+  return (
+    <main className="flex h-screen w-screen flex-col items-center justify-center gap-y-10">
+      <h1 className="text-xl">404 - Cette page n'existe pas.</h1>
+      <Link
+        to="/"
+        className="text-primary flex items-center gap-x-1 hover:underline"
+      >
+        <AiOutlineArrowLeft />
+        <span>Aller à l'accueil</span>
+      </Link>
+    </main>
+  );
 }
