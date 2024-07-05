145cd1cb334fefd5057ff3fdd347f2683c24189a
aubers <purplnay@gmail.com>
2024-07-05 16:44:57 +0200

    Add Kasa

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,8 +1,9 @@
 import { makeSlug } from "../lib/makeSlug";
 import { booki } from "./booki";
+import { kasa } from "./kasa";
 import { ninaCarducci } from "./nina-carducci";
 import { sophieBluel } from "./sophie-bluel";
 
-export const projects = [booki, ninaCarducci, sophieBluel].sort(
+export const projects = [booki, kasa, ninaCarducci, sophieBluel].sort(
   (a, b) => makeSlug(a.title).charCodeAt(0) - makeSlug(b.title).charCodeAt(0),
 );
---
+++ src/projects/kasa/index.tsx
@@ -0,0 +1,39 @@
+import { Project } from "../../lib/Project";
+import icon from "./icon.svg";
+import image1 from "./image-1.webp";
+import image2 from "./image-2.webp";
+import image3 from "./image-3.webp";
+import image4 from "./image-4.webp";
+import image5 from "./image-5.webp";
+
+export const kasa: Project = {
+  icon,
+  title: "Kasa",
+  description:
+    "Site web d'une agence de locations de vacances proposant de nombreux logements partout en France.",
+  url: "https://github.com/purplnay/kasa",
+  text: (
+    <>
+      <p>
+        Durant ma formation de Développeur Web chez{" "}
+        <a href="https://openclassrooms.com/" target="_blank">
+          OpenClassrooms
+        </a>
+        , j'ai du réaliser ce site à partir d'une maquette Figma. Le but de ce
+        projet était de développer une application <strong>React.js</strong>{" "}
+        complète, à partir d'une maquette, en utilisant aucune libraire hors
+        <strong>React Router</strong>.
+      </p>
+      <p>
+        Le site est codé totalement en JavaScript, en utilisant{" "}
+        <strong>React.js</strong>. Le routing est géré par{" "}
+        <strong>React Router</strong>, les données sont chargées depuis un
+        fichier JSON. Pour le CSS, <strong>SASS</strong> a été utilisé. Les
+        animations sont écrites directement en <strong>SASS</strong>, sans autre
+        librairie.
+      </p>
+    </>
+  ),
+  images: [image1, image2, image3, image4, image5],
+  tags: ["HTML", "CSS", "SASS", "JavaScript", "React.js"],
+};
