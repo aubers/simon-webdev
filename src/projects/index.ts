bfe0ac0de11e4815d1a0239a4980881c444bce60
aubers <purplnay@gmail.com>
2024-07-05 16:15:05 +0200

    Add Nina Carducci

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,7 +1,8 @@
 import { makeSlug } from "../lib/makeSlug";
 import { booki } from "./booki";
+import { ninaCarducci } from "./nina-carducci";
 import { sophieBluel } from "./sophie-bluel";
 
-export const projects = [booki, sophieBluel].sort(
+export const projects = [booki, ninaCarducci, sophieBluel].sort(
   (a, b) => makeSlug(a.title).charCodeAt(0) - makeSlug(b.title).charCodeAt(0),
 );
---
+++ src/projects/nina-carducci/index.tsx
@@ -0,0 +1,45 @@
+import { Project } from "../../lib/Project";
+import icon from "./icon.webp";
+import image1 from "./image-1.webp";
+import image2 from "./image-2.webp";
+import image3 from "./image-3.webp";
+
+export const ninaCarducci: Project = {
+  icon,
+  title: "Nina Carducci - Photographe",
+  description:
+    "Portfolio de la photographe Nina Carducci optimisé SEO pour un meilleur référencement.",
+  url: "https://github.com/purplnay/nina-carducci.github.io",
+  text: (
+    <>
+      <p>
+        Dans le cadre de la formation Développeur Web d'
+        <a href="https://openclassrooms.com" target="_blank">
+          OpenClassrooms
+        </a>
+        , <strong>l'optimisation</strong> d'un site web déjà existant.
+      </p>
+      <p>
+        Le portfolio a été analysé avec différents outils tels que{" "}
+        <a href="https://gtmetrix.com/">
+          <strong>GTmetrix</strong>
+        </a>
+        , <strong>Google Lighthouse</strong>, <strong>WAVE</strong> et{" "}
+        <strong>Color Contrast Analyzer</strong>. Cela a permis de voir les
+        points à améliorer pour le <strong>référencement</strong> du portfolio
+        ainsi que son
+        <strong>accessibilité</strong>.
+      </p>
+      <p>
+        Certains contrastes de couleurs ont été ajustés, l'ajout des metadonnées
+        pour les réseaux sociaux ainsi que les données{" "}
+        <a href="https://schema.org/" target="_blank">
+          Schema.org
+        </a>{" "}
+        ainsi que la correction de certains bugs.
+      </p>
+    </>
+  ),
+  images: [image1, image2, image3],
+  tags: ["JavaScript", "CSS", "SEO"],
+};
