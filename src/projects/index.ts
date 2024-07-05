e3ebaf3e475109c9d6ea87bbfe731a54497b22c5
aubers <purplnay@gmail.com>
2024-07-05 17:20:48 +0200

    Add P2P Screensharing

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -4,6 +4,7 @@ import { genshinTeamRandomizer } from "./genshin-team-randomizer";
 import { kasa } from "./kasa";
 import { monVieuxGrimmoire } from "./mon-vieux-grimmoire";
 import { ninaCarducci } from "./nina-carducci";
+import { p2pScreensharing } from "./p2p-screensharing";
 import { sophieBluel } from "./sophie-bluel";
 
 export const projects = [
@@ -12,6 +13,7 @@ export const projects = [
   kasa,
   monVieuxGrimmoire,
   ninaCarducci,
+  p2pScreensharing,
   sophieBluel,
 ].sort(
   (a, b) => makeSlug(a.title).charCodeAt(0) - makeSlug(b.title).charCodeAt(0),
---
+++ src/projects/p2p-screensharing/index.tsx
@@ -0,0 +1,60 @@
+import { Project } from "../../lib/Project";
+import icon from "./icon.webp";
+import desktopHomepage from "./desktop-homepage.webp";
+import desktopStream from "./desktop-stream.webp";
+import homepage from "./homepage.webp";
+import joiningRequest from "./joining-request.webp";
+import joiningWaiting from "./joining-waiting.webp";
+import streaming from "./streaming.webp";
+import waitingRoom from "./waiting-room.webp";
+
+export const p2pScreensharing: Project = {
+  icon,
+  title: "P2P Screensharing",
+  description:
+    "Une application de partage d'écran et discussion, en peer-to-peer.",
+  images: [
+    desktopHomepage,
+    desktopStream,
+    homepage,
+    joiningRequest,
+    joiningWaiting,
+    streaming,
+    waitingRoom,
+  ],
+  tags: ["Vue", "TypeScript", "TailwindCSS"],
+  url: "https://github.com/purplnay/p2p-screensharing",
+  text: (
+    <>
+      <p>
+        P2P Screensharing est une application web de{" "}
+        <strong>partage d'écran</strong>, en <strong>peer-to-peer</strong>, sans
+        aucun besoin de back-end. Elle permet à deux utilisateurs de se{" "}
+        <strong>rejoindre</strong> dans une{" "}
+        <strong>salle de discussion privée</strong>, ayant leurs appareils se
+        connectant directement entre-eux par internet. L'application permet de
+        <strong>diffuser son écran</strong>, une fenêtre ou un onglet, tout en
+        <strong>discutant</strong> via le chat.
+      </p>
+
+      <p>
+        L'application est <strong>open-source</strong> et disponible sur{" "}
+        <strong>
+          <a
+            href="https://github.com/purplnay/p2p-screensharing"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </strong>
+        . Elle est développée avec <strong>Vue.js</strong> dans un projet créé
+        avec <strong>Vite</strong>. L'UI est codée en utilisant{" "}
+        <strong>TailwindCSS</strong> ainsi que du <strong>CSS natif</strong>{" "}
+        pour certaines animations. Le langage utilisé est{" "}
+        <strong>TypeScript</strong> et la connexion en peer-to-peer est
+        effectuée avec la librairie <strong>PeerJS</strong>. L'application est
+        deployée sur <strong>Vercel</strong>.
+      </p>
+    </>
+  ),
+};
