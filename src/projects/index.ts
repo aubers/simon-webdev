e5a954d13030d983afffb96ba11bef99f7594941
aubers <purplnay@gmail.com>
2024-07-05 15:37:05 +0200

    Add Sophie Bluel

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,6 +1,7 @@
 import { makeSlug } from "../lib/makeSlug";
 import { booki } from "./booki";
+import { sophieBluel } from "./sophie-bluel";
 
-export const projects = [booki].sort(
+export const projects = [booki, sophieBluel].sort(
   (a, b) => makeSlug(a.title).charCodeAt(0) - makeSlug(b.title).charCodeAt(0),
 );
---
+++ src/projects/sophie-bluel/index.tsx
@@ -0,0 +1,42 @@
+import { Project } from "../../lib/Project";
+import icon from "./icon.webp";
+import image1 from "./image-1.webp";
+import image2 from "./image-2.webp";
+import image3 from "./image-3.webp";
+import image4 from "./image-4.webp";
+import image5 from "./image-5.webp";
+
+export const sophieBluel: Project = {
+  icon,
+  title: "Sohpie Bluel - Portfolio",
+  description: "Le portfolio de Sophie Bluel, architecte d'intérieur.",
+  url: "https://github.com/purplnay/portfolio-sophie-bluel",
+  text: (
+    <>
+      <p>
+        Projet réalisé durant la formation de Développeur Web d'
+        <a href="https://openclassrooms.com" target="_blank">
+          OpenClassrooms
+        </a>
+        . Le site contenait déjà le code du back-end en Node.js, ainsi que
+        l'intégration HTML et CSS. Les objectifs étaient de suivre les tâches du
+        <strong>Kaban</strong> afin d'y ajouter les parties{" "}
+        <strong>dynamiques</strong>.
+      </p>
+      <p>
+        L'utilisation des <strong>API DOM et fetch</strong> a permis de gérer
+        les travaux via l'API tout en gardant l'interface en{" "}
+        <strong>synchronisation</strong>
+        avec la base de données du back-end.
+      </p>
+      <p>
+        Le portfolio possède également un{" "}
+        <strong>système d'authentification</strong> afin que Sophie Bluel puisse
+        se connecter et mettre à jour les travaux disponibles via des{" "}
+        <strong>modales</strong>.
+      </p>
+    </>
+  ),
+  images: [image1, image2, image3, image4, image5],
+  tags: ["HTML", "CSS", "JavaScript"],
+};
