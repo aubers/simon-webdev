bc7afc88285352b280f3516427cf1880f1903093
aubers <purplnay@gmail.com>
2024-07-05 13:21:27 +0200

    Fix footer year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,9 @@ export default function Footer() {
   return (
     <footer className="mx-4 my-6 flex items-center justify-between lg:mx-0">
       <Socials />
-      <p className="text-sm text-white/80">© 2023 Simon Auber</p>
+      <p className="text-sm text-white/80">
+        © {new Date().getFullYear()} Simon Auber
+      </p>
     </footer>
   );
 }
